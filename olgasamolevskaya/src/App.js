5f81f93625c45295cb998091c360acb7d7b59e54
Marina Paskal <paskalm@rogers.com>
2022-10-18 15:58:05 -0400

    Add navbar

--- olgasamolevskaya/src/App.js
+++ olgasamolevskaya/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -11,14 +11,12 @@ const App = () => {
   return (
     <>
       <div className="app">
-        <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Contact" element={<Contact />} />
-            <Route path="/About" element={<About />} />
-          </Routes>
-        </BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/Контакти" element={<Contact />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
       </div>
     </>
   );
---
+++ olgasamolevskaya/src/components/Navigation.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function Navigation() {
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Navbar.Brand href="#">Ольга Самолевская</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link as={Link} href={"/"}>
+                Початок
+              </Nav.Link>
+              <NavDropdown title="Фільми" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#movie1">Фільми</NavDropdown.Item>
+                <NavDropdown.Item href="#movie2">Вірші</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="Movi">Фільми</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Вірші</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="Movi">Фільми</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Вірші</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Something else here
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <Nav.Link as={Link} href={"/"}>
+                Контакти
+              </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navigation;
