1e499aa06831fc4d80c0d12baa462ddec8b4bc54
Marina Paskal <paskalm@rogers.com>
2022-11-07 23:54:01 -0500

    Add links withing Poems page

--- olgasamolevskaya/src/App.js
+++ olgasamolevskaya/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Poems from "./pages/Poems";
@@ -21,6 +21,17 @@ const App = () => {
             <Route path="poems" element={<Poems />} />
             <Route path="articles" element={<Articles />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="poems/#schoolgirl" element={<Poems />} />
+            <Route path="poems/#landscape" element={<Poems />} />
+            <Route path="poems/#ikar" element={<Poems />} />
+            <Route path="poems/#boundless" element={<Poems />} />
+            <Route path="poems/#suddenly" element={<Poems />} />
+            <Route path="poems/#washed" element={<Poems />} />
+            <Route path="poems/#mid" element={<Poems />} />
+            <Route path="poems/#walked" element={<Poems />} />
+            <Route path="poems/#holdingon" element={<Poems />} />
+            <Route path="poems/#girl" element={<Poems />} />
+            <Route path="poems/#russian" element={<Poems />} />
           </Route>
         </Routes>
         <Footer year={new Date().getFullYear()} />
